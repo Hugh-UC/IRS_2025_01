0ce70005b03421ca2f60b8e0f7ab8fa2fda4d1a2
Hugh-UC <u3276400@uni.canberra.edu.au>
2025-09-07 13:24:07 +1000

    create: added hand_solo_virtual_nav package and initial warehouse map

--- hand_solo_virtual_nav/setup.py
+++ hand_solo_virtual_nav/setup.py
@@ -6,22 +6,20 @@ package_name = 'hand_solo_virtual_nav'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (
-            os.path.join('share', package_name, 'launch'), glob ('launch/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
         ('share/' + package_name + '/config', glob('config/*.yaml')),
-        ('share/' + package_name + '/rviz',
-        glob('rviz/*.rviz')),
+        ('share/' + package_name + '/rviz', glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
+    maintainer='group_1',
+    maintainer_email='u3276400@uni.canberra.edu.au',
     description='Hand Solo Virtual Navigation Package',
     license='Apache License 2.0',
     tests_require=['pytest'],
